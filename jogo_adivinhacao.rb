2bab42533d8ac3927c0a5ac69616c54ab2cfc8ff
Ronaldo Faustino <ohnirucse@msn.com>
2018-04-15 22:18:29 -0300

    Arrays

--- jogo_adivinhacao.rb
+++ jogo_adivinhacao.rb
@@ -2,9 +2,9 @@
  def da_boas_vindas #definindo função
     puts "Bem vindo ao jogo da adivinhação"
     puts "Qual é o seu nome?"
-    nome = gets
+    nome = gets.strip
     puts "\n\n\n"
-    puts "Começaremos o jogo para você, "+ nome
+    puts "Começaremos o jogo para você, #{nome}"
  end
  
  def sorteia_numero_secreto
@@ -14,8 +14,66 @@
     sorteado
  end
 
+ def pede_numero(chutes, tentativa, limite_tentativas)
+     puts "\n\n\n"    
+     puts "Tentativa #{tentativa} de #{limite_tentativas}"    
+     puts "Tentatita com os numero até agora #{chutes}"
+     puts "Entre com o numero"    
+     chute = gets.strip    
+     puts "Será que acertou? Você chutou #{chute}"  
+     chute.to_i   
+ end
+
+ def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute   
+    if acertou         
+        puts "Acertou!"
+        #break
+        return true
+    end    
+        maior = numero_secreto > chute
+        if maior            
+            puts "O número secreto é maior!"
+        else
+            puts "O número secreto é menor!"
+        end
+        false
+ end
+
  da_boas_vindas
  numero_secreto = sorteia_numero_secreto
+ 
+ limite_tentativas = 5 
+ chutes = []
+  
+ for tentativa in 1..limite_tentativas    
+     
+    chute = pede_numero chutes, tentativa, limite_tentativas      
+    chutes << chute
+     
+    if verifica_se_acertou numero_secreto, chute
+     break
+     end
+ end
+
+#  def da_boas_vindas #definindo função
+#     puts "Bem vindo ao jogo da adivinhação"
+#     puts "Qual é o seu nome?"
+#     nome = gets.strip
+#     puts "\n\n\n"
+#     puts "Começaremos o jogo para você, "+ nome
+#  end
+
+# def pede_numero(chutes, tentativa, limite_tentativas)
+#     puts "\n\n\n"    
+#     puts "Tentativa "+ tentativa.to_s + " de "+  limite_tentativas.to_s    
+#     puts "Tentatita com os numero até agora"+ chutes.to_s
+#     puts "Entre com o numero"    
+#     chute = gets.strip    
+#     puts "Será que acertou? Você chutou "+ chute  
+#     chute.to_i   
+# end
+
  # for tentativa in1..3    
  #     puts "\n\n\n\n"    
  #     puts "Tentativa 1"    
@@ -32,26 +90,42 @@
  #Mas sempre imprime Tentativa 1. 
  #Corrigimos para mostrar atentativa certa junto com o total de tentativas:
  # ...
- limite_tentativas = 5 
- for tentativa in 1..limite_tentativas    
-     puts "\n\n\n"    
-     puts "Tentativa "+ tentativa.to_s + " de "+  limite_tentativas.to_s    
-     puts "Entre com o numero"    
-     chute = gets    
-     puts "Será que acertou? Você chutou "+ chute    
+
+#  def verifica_se_acertou(numero_secreto, chute)
+#     acertou = numero_secreto == chute   
+#     if acertou         
+#         puts "Acertou!"
+#         #break
+#         return true
+#     else        
+#         maior = numero_secreto > chute
+#         if maior            
+#             puts "O número secreto é maior!"
+#             return false
+#         else
+#             puts "O número secreto é menor!"
+#             return false
+#         end
+#     end
+#  end
+
+# for tentativa in 1..limite_tentativas    
+#     chute = pede_numero tentativa, limite_tentativas      
+#     if verifica_se_acertou numero_secreto, chute
+#        break
+#     end
+  
+# end
+
+# total_de_chutes = 0
+ 
+# for tentativa in 1..limite_tentativas    
+    
+#    chute = pede_numero chutes, tentativa, limite_tentativas      
+#    chutes[total_de_chutes] = chute
+#    total_de_chutes = total_de_chutes + 1
    
-     acertou = numero_secreto == chute.to_i
-   
-     if acertou         
-         puts "Acertou!"
-         break
-     else        
-         maior = numero_secreto > 
-         chute.to_i
-         if maior            
-             puts "O número secreto é maior!"
-         else
-             puts "O número secreto é menor!"
-         end
-     end
- end
+#    if verifica_se_acertou numero_secreto, chute
+#     break
+#     end
+# end
---
+++ testes.rb
@@ -0,0 +1,29 @@
+# chutes = [176, 100, 150, 175]
+# chutes [3] = 199
+
+# puts chutes [0]
+# puts chutes [1]
+# puts chutes [2]
+# puts chutes [3]
+
+# chutes = []
+
+# chute = 176
+# chutes << chute
+
+# chute = 170
+# chutes << chute
+
+# chute = 175
+# chutes << chute
+
+# puts chutes.size
+# for chute in chutes
+#     puts chute
+# end
+
+nome = gets.strip
+puts "#{nome} tem #{nome.size} caractere"
+# puts "Seu nome tem "+ nome.size.to_s + " Caractere"
+puts "Resultado"
+puts nome[9]
